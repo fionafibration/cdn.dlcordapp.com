77e34f617db7788c6a2d0b750f92dd903be66ad9
Finian Blackett <spamsuckersunited@gmail.com>
2020-02-13 12:55:26 -0700

    Remove app.py why is this so annoying I really need dotenvs reeeee

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from flask import Flask, escape, request, redirect
 
 app = Flask(__name__)
 
-ua_patterns = ['DiscordBot', '+https://discordapp.com', 'electron', 'firefox/38']
+ua_patterns = ['DiscordBot', '+https://discordapp.com', 'electron']
 
-
-# no longer shitty
+# Crappy way to detect if we're getting indexed by the Discord web crawler for embedding
 def is_embed():
     ua_string = request.user_agent.string
 
-    return [pattern in ua_string for pattern in ua_patterns]
+    return any([pattern in ua_string for pattern in ua_patterns])
 
 
 
