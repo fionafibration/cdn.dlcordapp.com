8a14e44a25b43b2922b97a7fa450f29e181850f5
Max Paulson <novalogicdev@gmail.com>
2020-02-13 09:05:50 -0700

    added hello.py

---
+++ hello.py
@@ -0,0 +1,8 @@
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
