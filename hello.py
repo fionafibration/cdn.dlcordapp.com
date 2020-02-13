b79efa4ccbf29d8835565e50a5e9f0c51afaaf6a
Max Paulson <novalogicdev@gmail.com>
2020-02-13 10:01:34 -0700

    added vscode config shit

--- hello.py
+++ hello.py
@@ -5,4 +5,5 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    agent = request.headers.get('User-Agent')
+    return f'Hello, {escape(name)}! {escape()}'
